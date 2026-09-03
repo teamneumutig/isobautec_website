d3872ded2fe690fba7b657f1e99e3e28543cb56b
teamneumutig <team.neumutig@gmail.com>
2026-09-03 14:40:04 +0200

    Use Contao sortable file tree option

--- rsce_ib_projects_config.php
+++ rsce_ib_projects_config.php
@@ -104,7 +104,7 @@ return [
                     'eval' => [
                         'multiple' => true,
                         'fieldType' => 'checkbox',
-                        'orderField' => 'sorting',
+                        'isSortable' => true,
                         'filesOnly' => true,
                         'extensions' => 'jpg,jpeg,png,webp,avif',
                         'tl_class' => 'clr',
